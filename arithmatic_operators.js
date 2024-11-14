7227305b685e61df3b674f3269d14717ef31b292
Mustafa Osmanoglu <143639628+Mustafa-Osmanoglu@users.noreply.github.com>
2024-11-14 18:16:19 +0300

    Create arithmatic_operators.js

---
+++ arithmatic_operators.js
@@ -0,0 +1,28 @@
+let a = 20
+let b = 6
+
+let c = a % b
+
+console.log("c =" , c)
+
+c = a ** b
+
+console.log("c = ", c)
+
+c **= c
+
+console.log("c = ", c)
+
+c = 10
+
+c++
+
+console.log("c = ", c)
+
+c--
+
+console.log("c = ", c)
+
+c += a // result should be 30 c = c+a mathematically
+
+console.log("c = ", c)
