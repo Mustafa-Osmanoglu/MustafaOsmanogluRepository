5bae020825b6bb3b218335def6d3eda57695ae4c
Mustafa Osmanoglu <143639628+Mustafa-Osmanoglu@users.noreply.github.com>
2024-11-14 18:20:47 +0300

    Create logical_and_comparison_operators.js

---
+++ logical_and_comparison_operators.js
@@ -0,0 +1,20 @@
+
+// && and  ,  || or , ! not
+let a = true
+
+let b = false
+
+
+console.log( a && b)
+
+
+console.log( a || b)
+
+console.log (!a)
+
+let x=100
+let y=200
+
+console.log(x<y)
+
+console.log(x >= y)
