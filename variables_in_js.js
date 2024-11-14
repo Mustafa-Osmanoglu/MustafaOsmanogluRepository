df603b65965f48ed3c8d0b2a08dd807107d145c0
Mustafa Osmanoglu <143639628+Mustafa-Osmanoglu@users.noreply.github.com>
2024-11-14 18:10:54 +0300

    Create variables_in_js.js

---
+++ variables_in_js.js
@@ -0,0 +1,45 @@
+// let
+// Type Can Change
+// var
+// BigInt
+// Symbol
+// we can use let one time, var multiple declare
+
+
+let a = 100
+
+let str = "I like Node JS"
+
+let b = true
+
+str = "I like learning Node JS"
+
+
+a = false
+
+var d = "ABC"
+
+console.log("Value of d: ", d)
+console.log("Type of d: ", typeof(d))
+
+
+var d = 200
+
+console.log("Value of d: ", d)
+console.log("Type of d: ", typeof(d))
+
+
+let x = BigInt (1231242135)
+
+let y = 123431245321523525n
+
+console.log("Value of x: ", x)
+console.log("Type of x: ", typeof(x))
+
+console.log("Value of y: ", y)
+console.log("Type of y: ", typeof(y))
+
+let s = Symbol('$')
+
+console.log("Value of s: ", s)
+console.log("Type of s:", typeof(s))
