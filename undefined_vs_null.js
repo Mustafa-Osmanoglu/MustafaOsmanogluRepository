003f30beb5111ddcfd6f639fc087882ae6bffa11
Mustafa Osmanoglu <143639628+Mustafa-Osmanoglu@users.noreply.github.com>
2024-11-14 18:15:19 +0300

    Create undefined_vs_null.js

---
+++ undefined_vs_null.js
@@ -0,0 +1,16 @@
+//Undefined is a Type
+//Null is a value
+
+    let a
+
+    console.log("Type of a: " + typeof(a))
+
+    console.log("Value of a: " + a)
+
+
+    let b = null
+
+
+    console.log("Type of b: " + typeof(b))
+
+    console.log("Value of b: " + b)
