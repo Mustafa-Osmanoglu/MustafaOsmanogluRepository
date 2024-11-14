7dc6cfda4b366fed8990dc2482d50dd5986a485f
Mustafa Osmanoglu <143639628+Mustafa-Osmanoglu@users.noreply.github.com>
2024-11-14 18:22:39 +0300

    Create bitwise_operations.js

---
+++ bitwise_operations.js
@@ -0,0 +1,18 @@
+
+
+let a=50 // 0110010
+
+let b=100 // 1100100
+
+let c = a&b
+
+console.log(c)
+
+c = a|b
+
+console.log(c)
+
+c = ~b  // convert + to - , - to + and decrease one value
+
+
+console.log(c)
